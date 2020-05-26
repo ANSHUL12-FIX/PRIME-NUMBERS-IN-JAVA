ff2ed8e3b9937cc9a5b519944bdba4505499bf1e
ANSHUL12-FIX <64637596+ANSHUL12-FIX@users.noreply.github.com>
2020-05-26 11:27:18 +0530

    Update fibo.java

--- fibo.java
+++ fibo.java
@@ -12,7 +12,7 @@ public class Fibo {
     public static void main(String[] args) {
         // TODO code application logic here
         int a=0,b=1,i,c;
-        System.out.println("ENTER NUMBERS");
+        System.out.println("fibonacci series");
         for(i=2;i<10;i++){
            c=a+b;
            System.out.println(""+c);
