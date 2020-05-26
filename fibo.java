4dec417bd2a58b454e5d1be4aecdcfdd150f1a6f
ANSHUL12-FIX <64637596+ANSHUL12-FIX@users.noreply.github.com>
2020-05-26 11:11:17 +0530

    fibo.java
    fibonacci series proh=gram in java

---
+++ fibo.java
@@ -0,0 +1,24 @@
+package fibo;
+
+/**
+ *
+ * @author anshul
+ */
+public class Fibo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a=0,b=1,i,c;
+        System.out.println("ENTER NUMBERS");
+        for(i=2;i<10;i++){
+           c=a+b;
+           System.out.println(""+c);
+           a=b;
+           b=c; 
+        }
+    }
+    
+}
